84311b152e99183d901944f8e5812666c1786135
Tobias Spiller <54402019+TobiasRSpiller@users.noreply.github.com>
2023-12-11 22:34:48 +0100

    Add files via upload

--- time.js
+++ time.js
@@ -2,7 +2,9 @@ function updateDisplayedTime() {
     let now = new Date();
     // Convert to Zurich time
     let zurichTime = new Date(now.toLocaleString("en-US", { timeZone: "Europe/Zurich" }));
-    document.getElementById('time').innerText = zurichTime.toLocaleTimeString();
+    // Format the time to show only hours and minutes
+    let formattedTime = zurichTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    document.getElementById('time').innerText = formattedTime;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
